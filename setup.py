bfed086d996f9755a2b9f57127ad41dac1abe816
FlorianLatestere <62744834+FlorianLatestere@users.noreply.github.com>
2020-04-22 15:14:00 +0200

    renomage

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='IN104_PROJECT_INSERT_YOUR_GROUP_NAMES_HERE',
+setup(name='IN104_PROJECT_CHOLLET_LATESTERE',
       description='IN104 Project package',
       author='<INSERT_YOUR_GROUP_NAMES_HERE>',
       packages=find_packages()
