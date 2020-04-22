8068f344a89d176410c4e60a758457af3b707a4d
Florian Latestere <florian.latestere@ensta-paris.fr>
2020-04-22 15:04:30 +0200

    first

---
+++ IN104_PROJECT_squeleton/scripts/human_vs_AI.py
@@ -0,0 +1,6 @@
+# executez ce script dans un terminal (depuis n'importe quel repertoire)
+# avec la commande python -m IN104_PROJECT_NOM1_NOM2.scripts.human_vs_AI
+import aiarena
+from ..minimaxBrain import MinimaxBrain
+
+# Lancer une partie entre votre IA MinimaxBrain et un humain sur le puissance4 ou aux dames
