91d73f2776dd67941268ef8ae5dbb3320ddb30b2
FlorianLatestere <62744834+FlorianLatestere@users.noreply.github.com>
2020-04-22 16:24:17 +0200

    rand1

--- IN104_PROJECT_squeleton/randomBrain.py
+++ IN104_PROJECT_squeleton/randomBrain.py
@@ -2,10 +2,15 @@ import random
 
 class RandomBrain:
     def __init__(self):
-        pass
+        self.name = OrdiRand
+        self.alwaysSeeAsWhite = False
 
     def play(self, gameState, timeLimit):
-        raise NotImplementedError()
+        possibleMoves = gameState.findPossibleMoves()
+        n=len(possibleMoves)
+        gameState.display(showBoard=True)
+        a=random.randint(0,n)
+        return possibleMoves[a]
 
     def __str__(self):
         return "Random_Player"
