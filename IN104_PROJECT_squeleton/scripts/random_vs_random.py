26799a54629f1d3a04df0baa9835a950b7cafd7f
tomchol <62744934+tomchol@users.noreply.github.com>
2020-04-22 16:32:26 +0200

    Fix bug 2 import randomBrain

--- IN104_PROJECT_squeleton/scripts/random_vs_random.py
+++ IN104_PROJECT_squeleton/scripts/random_vs_random.py
@@ -1,13 +1,12 @@
 # executez ce script dans un terminal (depuis n'importe quel repertoire)
 # avec la commande python -m IN104_PROJECT_NOM1_NOM2.scripts.random_vs_random
 import aiarena
-import randomBrain
 from ..randomBrain import RandomBrain
 
 # TODO: Instantier ICI des IA de type RandomBrain
 
-brain1 = randomBrain.RandomBrain()
-brain2 = randomBrain.RandomBrain()
+brain1 = RandomBrain()
+brain2 = RandomBrain()
 timeLimit = 1 #The AI gave 1 second to play
 
 for module in [aiarena.abalone, aiarena.chess, aiarena.checkers, aiarena.connect4]:
