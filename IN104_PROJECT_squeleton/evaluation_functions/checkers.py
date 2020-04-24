9ccaa2feb0e1026f5116adab06b97519f8fcc586
Florian Latestere <florian.latestere@ensta-paris.fr>
2020-04-24 14:35:48 +0200

    eval checkers

--- IN104_PROJECT_squeleton/evaluation_functions/checkers.py
+++ IN104_PROJECT_squeleton/evaluation_functions/checkers.py
@@ -1,4 +1,19 @@
 from aiarena.checkers import cell
 
 def evaluate(gameState):
-    raise NotImplementedError()
+	score=0
+    for i in range (8):
+    	for j in range (8):
+    		if gameState.getCell(i,j).type = checkers.cell.MAN:
+    			if gameState.getCell(i,j).isWhite:
+    				score=score+1
+    			else:
+    				score=score-1
+    		if gameState.getCell(i,j).type = checkers.cell.KING:
+    			if gameState.getCell(i,j).isWhite:
+    				score=score+3
+    			else:
+    				score=score-3
+    return (score)
+
+
