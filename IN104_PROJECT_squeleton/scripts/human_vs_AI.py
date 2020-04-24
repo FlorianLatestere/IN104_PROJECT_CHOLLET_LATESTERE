9a6dc3869dd0eb177190b956c5849da4cb614796
Florian Latestere <florian.latestere@ensta-paris.fr>
2020-04-24 14:52:29 +0200

    script ai human

--- IN104_PROJECT_squeleton/scripts/human_vs_AI.py
+++ IN104_PROJECT_squeleton/scripts/human_vs_AI.py
@@ -3,4 +3,13 @@
 import aiarena
 from ..minimaxBrain import MinimaxBrain
 
+brain1 = aiarena.ManualBrain()
+human_time = 20 #the human will have 10 secs to play
+brain2 = MinimaxBrain()
+ai_time = 1 #the AI will only have 1 sec to play
+game = aiarena.Game(aiarena.chess, brain1, human_time, brain2, ai_time)
+game.displayLevel = 1   # this prints the board after each move
+game.start()
+print(game.pgn) #print the summary of the game. 
+
 # Lancer une partie entre votre IA MinimaxBrain et un humain sur le puissance4 ou aux dames
