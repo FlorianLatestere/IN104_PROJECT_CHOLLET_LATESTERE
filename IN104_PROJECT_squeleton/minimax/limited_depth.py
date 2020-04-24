9b664dd4ac58ee184383a7ccc9ebd2bd808ee828
tomchol <62744934+tomchol@users.noreply.github.com>
2020-04-24 15:13:06 +0200

    Update limited_depth.py

--- IN104_PROJECT_squeleton/minimax/limited_depth.py
+++ IN104_PROJECT_squeleton/minimax/limited_depth.py
@@ -1,7 +1,9 @@
 def minimax(node, maximize, get_children, evaluate, max_depth):
 	if (max_depth == 0):
 		return evaluate(node)
-	nouedsEnfant = get_children(node)
+	noeudsEnfant = get_children(node)
+	if len(noeudsEnfant == 0) :
+		return evaluate(node)
 	minmaxEnfant = []
 	for element in noeudsEnfant :
 		minmaxEnfant.append(minimax(element, not maximize, get_children, evaluate,max_depth-1))
