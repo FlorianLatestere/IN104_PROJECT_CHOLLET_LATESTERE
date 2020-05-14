a0c052c701059b7046c97f2d52ec16eaeffd803e
Florian Latestere <florian.latestere@ensta-paris.fr>
2020-05-14 09:33:14 +0200

    ajout aivsai

---
+++ IN104_PROJECT_CHOLLET_LATESTERE/scripts/ai_vs_ai.py
@@ -0,0 +1,16 @@
+# executez ce script dans un terminal (depuis n'importe quel repertoire)
+# avec la commande python -m IN104_PROJECT_NOM1_NOM2.scripts.human_vs_AI
+import aiarena
+from ..minimaxBrain import MinimaxBrain
+
+brain1 = MinimaxBrain(aiarena.checkers)
+brain1.depth = 7
+brain2 = MinimaxBrain(aiarena.checkers)
+brain2.depth = 7
+ai_time = 1 #the AI will only have 1 sec to play
+game = aiarena.Game(aiarena.checkers, brain1, ai_time, brain2, ai_time)
+game.displayLevel = 1   # this prints the board after each move
+game.start()
+print(game.pgn) #print the summary of the game. 
+
+# Lancer une partie entre votre IA MinimaxBrain et un humain sur le puissance4 ou aux dames
