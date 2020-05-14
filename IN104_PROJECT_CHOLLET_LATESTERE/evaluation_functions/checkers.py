9f8d68ebd654097553812d971a3bd49a43e83d8c
chollet <tomchollet2000@gmail.com>
2020-05-14 08:54:55 +0200

    humain_vs_AI update

--- IN104_PROJECT_CHOLLET_LATESTERE/evaluation_functions/checkers.py
+++ IN104_PROJECT_CHOLLET_LATESTERE/evaluation_functions/checkers.py
@@ -4,12 +4,12 @@ def evaluate(gameState):
     score=0
     for i in range (8):
     	for j in range (8):
-    		if gameState.getCell(i,j).type = checkers.cell.MAN:
+    		if gameState.getCell(i,j).type == checkers.cell.MAN:
     			if gameState.getCell(i,j).isWhite:
     				score=score+1
     			else:
     				score=score-1
-    		if gameState.getCell(i,j).type = checkers.cell.KING:
+    		if gameState.getCell(i,j).type == checkers.cell.KING:
     			if gameState.getCell(i,j).isWhite:
     				score=score+3
     			else:
--- IN104_PROJECT_CHOLLET_LATESTERE/scripts/human_vs_AI.py
+++ IN104_PROJECT_CHOLLET_LATESTERE/scripts/human_vs_AI.py
@@ -5,9 +5,10 @@ from ..minimaxBrain import MinimaxBrain
 
 brain1 = aiarena.ManualBrain()
 human_time = 20 #the human will have 10 secs to play
-brain2 = MinimaxBrain()
+brain2 = MinimaxBrain(aiarena.checkers)
+brain2.depth = 7
 ai_time = 1 #the AI will only have 1 sec to play
-game = aiarena.Game(aiarena.chess, brain1, human_time, brain2, ai_time)
+game = aiarena.Game(aiarena.checkers, brain1, human_time, brain2, ai_time)
 game.displayLevel = 1   # this prints the board after each move
 game.start()
 print(game.pgn) #print the summary of the game. 
