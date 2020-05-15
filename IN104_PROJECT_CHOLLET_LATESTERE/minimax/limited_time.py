e590db627dcb8a820e65728142500c45c65dfc67
Florian Latestere <florian.latestere@ensta-paris.fr>
2020-05-15 15:09:17 +0200

    limited time

--- IN104_PROJECT_CHOLLET_LATESTERE/minimax/limited_time.py
+++ IN104_PROJECT_CHOLLET_LATESTERE/minimax/limited_time.py
@@ -1,2 +1,17 @@
-def minimax(node, maximize, get_children, evaluate, time):
-    raise NotImplementedError()
+def minimax(node, maximize, get_children, evaluate, T_limit,T_recherche):
+
+	if (T_recherche>T_limit):
+		return evaluate(node)
+	noeudsEnfant = get_children(node)
+
+	if len(noeudsEnfant) == 0 :
+		return evaluate(node)
+
+	minmaxEnfant = []
+	for element in noeudsEnfant :
+		minmaxEnfant.append(minimax(element, not maximize, get_children, evaluate,T_limit/len(noeudsEnfant),T_recherche)
+
+	if (maximize) :
+		return max(minmaxEnfant)
+	return min(minmaxEnfant)
+		
