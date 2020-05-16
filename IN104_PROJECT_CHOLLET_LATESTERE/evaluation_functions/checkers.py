8a29721414530877f77531d730517a6701d95b37
Florian Latestere <florian.latestere@ensta-paris.fr>
2020-05-16 21:45:06 +0200

    evaluation dames

--- IN104_PROJECT_CHOLLET_LATESTERE/evaluation_functions/checkers.py
+++ IN104_PROJECT_CHOLLET_LATESTERE/evaluation_functions/checkers.py
@@ -7,14 +7,18 @@ def evaluate(gameState):
     		if (j%2 == 1 and i%2 == 0) or (j%2 == 0 and i%2 == 1):  
     			if gameState.getCell(i,j).type == cell.MAN:
     				if gameState.getCell(i,j).isWhite:
-    					score=score+1
+    					score=score+val_case(i,j)
     				else:
-    					score=score-1
+    					score=score-val_case(i,j)
     			if gameState.getCell(i,j).type == cell.KING:
     				if gameState.getCell(i,j).isWhite:
-    					score=score+3
+    					score=score+2*val_case(i,j)
     				else:
-    					score=score-3
+    					score=score-2*val_case(i,j)
     return (score)
 
+
+def val_case(i,j):
+    val=[[0,100,0,100,0,100,0,80],[50,0,70,0,70,0,70,0],[0,25,0,25,0,25,0,40],[25,0,10,0,10,0,10,0],[0,10,0,10,0,10,0,25],[40,0,25,0,25,0,25,0],[0,70,0,70,0,70,0,50],[80,0,100,0,100,0,100,0]]
+    return (val[i][j])
 
