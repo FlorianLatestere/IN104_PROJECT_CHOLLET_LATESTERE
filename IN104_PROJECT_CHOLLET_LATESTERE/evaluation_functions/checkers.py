cc09feff96ca83450c2bd7438de26fa845b8e08c
Florian Latestere <florian.latestere@ensta-paris.fr>
2020-05-18 11:05:53 +0200

    checkers

--- IN104_PROJECT_CHOLLET_LATESTERE/evaluation_functions/checkers.py
+++ IN104_PROJECT_CHOLLET_LATESTERE/evaluation_functions/checkers.py
@@ -2,23 +2,37 @@ from aiarena.checkers import cell
 
 def evaluate(gameState):
     score=0
+    totBlanc=0
+    totNoir=0
     for i in range (8):
     	for j in range (8):
     		if (j%2 == 1 and i%2 == 0) or (j%2 == 0 and i%2 == 1):  
     			if gameState.getCell(i,j).type == cell.MAN:
     				if gameState.getCell(i,j).isWhite:
-    					score=score+val_case(i,j)
+    					score=score+5+i
+    					totBlanc=totBlanc+1
     				else:
-    					score=score-val_case(i,j)
+    					score=score-5-8+i
+    					totNoir=totNoir+1
+                                        
+
     			if gameState.getCell(i,j).type == cell.KING:
+
     				if gameState.getCell(i,j).isWhite:
-    					score=score+2*val_case(i,j)
+    					score=score+7+i
+    					totBlanc=totBlanc+1
     				else:
-    					score=score-2*val_case(i,j)
+    					score=score-7-8+i
+    					totNoir=totNoir+1
+                        		
+    if totNoir==0:
+        score=score+1000
+    if totBlanc==0:
+        score=score-1000
     return (score)
 
-
-def val_case(i,j):
-    val=[[0,100,0,100,0,100,0,80],[50,0,70,0,70,0,70,0],[0,25,0,25,0,25,0,40],[25,0,10,0,10,0,10,0],[0,10,0,10,0,10,0,25],[40,0,25,0,25,0,25,0],[0,70,0,70,0,70,0,50],[80,0,100,0,100,0,100,0]]
-    return (val[i][j])
-
+###
+#def val_case(i,j):
+#    val=[[0,100,0,100,0,100,0,80],[50,0,70,0,70,0,70,0],[0,25,0,25,0,25,0,40],[25,0,10,0,10,0,10,0],[0,10,0,10,0,10,0,25],[40,0,25,0,25,0,25,0],[0,70,0,70,0,70,0,50],[80,0,100,0,100,0,100,0]]
+#    return (val[i][j])
+###
--- IN104_PROJECT_CHOLLET_LATESTERE/scripts/ai_vs_ai.py
+++ IN104_PROJECT_CHOLLET_LATESTERE/scripts/ai_vs_ai.py
@@ -2,13 +2,13 @@
 # avec la commande python -m IN104_PROJECT_NOM1_NOM2.scripts.human_vs_AI
 import aiarena
 from ..minimaxTimeBrain import MinimaxBrain
-
-brain1 = MinimaxBrain(aiarena.connect4)
+from ..randomBrain import RandomBrain
+brain1 = MinimaxBrain(aiarena.checkers)
 brain1.depth = 5
-brain2 = MinimaxBrain(aiarena.connect4)
+brain2 = RandomBrain()
 brain2.depth = 5
 ai_time = 2 #the AI will only have 1 sec to play
-game = aiarena.Game(aiarena.connect4, brain1, ai_time, brain2, ai_time)
+game = aiarena.Game(aiarena.checkers, brain1, ai_time, brain2, ai_time)
 game.displayLevel = 1   # this prints the board after each move
 game.start()
 print(game.pgn) #print the summary of the game. 
